b4a2bcb7257a863bc6b8b6e8bd6511892a835cc9
Deyvisson Alex <deyvissonalex@gmail.com>
2016-11-03 14:19:19 -0200

    Mudanças sutis

---
+++ app/src/main/java/br/pucrs/petinf/visitasguiadas/FACIN.java
@@ -0,0 +1,13 @@
+package br.pucrs.petinf.visitasguiadas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FACIN extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facin);
+    }
+}
